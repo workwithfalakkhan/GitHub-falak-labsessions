17ac38eb6ba4de671690d29d10ffcca40e52e4f4
falak <work.with.falakkhan@gmail.com>
2026-07-27 07:41:02 +0530

    init commit

---
+++ src/main/java/com/practice/scalerlabsession/ScalerLabSessionApplication.java
@@ -0,0 +1,13 @@
+package com.practice.scalerlabsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerLabSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalerLabSessionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/scalerlabsession/ScalerLabSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.scalerlabsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerLabSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
